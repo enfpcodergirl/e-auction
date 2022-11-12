13e0c4f9df4f0e5133cbeb80a6c7ca051ffcc18e
enfpcodergirl <enfpcodergirl@gmail.com>
2022-11-12 19:19:46 +0530

    e-auction-seller initial commit

---
+++ e-auction-seller-service/src/test/java/com/fse/EAuctionSellerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAuctionSellerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
